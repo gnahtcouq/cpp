ec1dba6ff0c9f28128930c7179e573059d555d25
Quốc Thắng <comehere.thang@gmail.com>
2021-11-24 22:53:26 +0700

    Update Thang_DH52007101_B5.cpp
    24/11/21

--- Thuc Hanh Nhap Mon Cau Truc Du Lieu/buoi5/Thang_DH52007101_B5.cpp
+++ Thuc Hanh Nhap Mon Cau Truc Du Lieu/buoi5/Thang_DH52007101_B5.cpp
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <iomanip>
 #include <string.h>
@@ -30,7 +29,7 @@ void xoaCuoi(Nodeptr &list);
 void xoa_Sv_Masv(Nodeptr &list, SinhVien sv);
 void xoa_Sv_DiemDuoi5(Nodeptr &list);
 void reverseList_SV(Nodeptr &list);
-void splitList_SinhVien(Nodeptr &list, Nodeptr &list1, Nodeptr &list2);
+void splitList_SinhVien(Nodeptr list, Nodeptr &list1, Nodeptr &list2);
 void nhapThemDiem(Nodeptr &list);
 void sapXepDanhSach(Nodeptr &list);
 void xuat_DSSVNew(Nodeptr list);
@@ -42,49 +41,45 @@ int main()
     nhapDSSV(dssv);
     xuatDSSV(dssv);
 
-    // // Cau 3
-    // cout << "\nCau3. Nhap Ma SV can tim: ";
-    // cin.getline(sv.masv, 10);
-    // Nodeptr p = tim_Sv_Masv(dssv, sv);
-    // if (p == NULL)
-    // {
-    //     cout << "\nCau3. Khong tim thay";
-    // }
-    // else
-    // {
-    //     cout << "\nCau3. Co sinh vien: ";
-    //     cout << setw(0) << p->data.masv << setw(15) << p->data.tensv << setw(20) << p->data.diem;
-    // }
+    // Cau 3
+    cout << "\nCau3. Nhap Ma SV can tim: ";
+    cin.getline(sv.masv, 10);
+    Nodeptr p = tim_Sv_Masv(dssv, sv);
+    if (p == NULL)
+        cout << "\nCau3. Khong tim thay";
+    else
+    {
+        cout << "\nCau3. Co sinh vien: ";
+        cout << setw(0) << p->data.masv << setw(15) << p->data.tensv << setw(20) << p->data.diem;
+    }
 
-    // // Cau 4
-    // cout << "\nCau4. Danh sach sinh vien con lai sau khi xoa dau: " << endl;
-    // xoaDau(dssv);
-    // xuatDSSV(dssv);
+    // Cau 4
+    cout << "\nCau4. Danh sach sinh vien con lai sau khi xoa dau: " << endl;
+    xoaDau(dssv);
+    xuatDSSV(dssv);
 
-    // cout << "\nCau4. Danh sach sinh vien con lai sau khi xoa cuoi: " << endl;
-    // xoaCuoi(dssv);
-    // xuatDSSV(dssv);
+    cout << "\nCau4. Danh sach sinh vien con lai sau khi xoa cuoi: " << endl;
+    xoaCuoi(dssv);
+    xuatDSSV(dssv);
 
-    // // Cau 5
-    // cout << "\nCau5. Nhap ma sinh vien can xoa: ";
-    // cin.getline(sv.masv, 10);
-    // xoa_Sv_Masv(dssv, sv);
-    // cout << "\nCau5. Danh sach sinh vien sau khi xoa: " << endl;
-    // xuatDSSV(dssv);
+    // Cau 5
+    cout << "\nCau5. Nhap ma sinh vien can xoa: ";
+    cin.getline(sv.masv, 10);
+    xoa_Sv_Masv(dssv, sv);
+    cout << "\nCau5. Danh sach sinh vien sau khi xoa: " << endl;
+    xuatDSSV(dssv);
 
-    // cout << "\nCau5. Danh sach sinh vien sau khi xoa cac sinh vien co diem < 5 la: " << endl;
-    // xoa_Sv_DiemDuoi5(dssv);
-    // xuatDSSV(dssv);
+    cout << "\nCau5. Danh sach sinh vien sau khi xoa cac sinh vien co diem < 5 la: " << endl;
+    xoa_Sv_DiemDuoi5(dssv);
+    xuatDSSV(dssv);
 
-    // // BTVN Cau 1
-    // cout << "\nBTVN Cau1. Danh sach sinh vien sau khi dao nguoc la: " << endl;
-    // reverseList_SV(dssv);
-    // xuatDSSV(dssv);
+    // BTVN Cau 1
+    cout << "\nBTVN Cau1. Danh sach sinh vien sau khi dao nguoc la: " << endl;
+    reverseList_SV(dssv);
+    xuatDSSV(dssv);
 
     // BTVN Cau 2
-    khoiTao(dssv1);
-    khoiTao(dssv2);
-    cout << "\nBTVN Cau2. Tach danh sach sinh vien: ";
+    cout << "\nBTVN Cau2. Tach danh sach sinh vien";
     splitList_SinhVien(dssv, dssv1, dssv2);
     cout << "\nDanh sach sinh vien co diem < 5 la: " << endl;
     xuatDSSV(dssv1);
@@ -159,7 +154,7 @@ void nhapDSSV(Nodeptr &list)
     khoiTao(list);
     do
     {
-        cout << "\n Nhap MaSV (NHAP 0 DE THOAT): ";
+        cout << "\nNhap Ma SV (NHAP 0 DE THOAT): ";
         cin.getline(sv.masv, 10);
         while (kiemTra_TrungMa(list, sv.masv))
         {
@@ -325,16 +320,24 @@ void reverseList_SV(Nodeptr &list)
 }
 
 // BTVN Cau 2
-void splitList_SinhVien(Nodeptr &list, Nodeptr &list1, Nodeptr &list2)
+void splitList_SinhVien(Nodeptr list, Nodeptr &list1, Nodeptr &list2)
 {
-    while (list != NULL)
-    {
-        if (list->data.diem < 5)
-            themCuoi(list1, list->data);
-        else
-            themCuoi(list2, list->data);
-        list = list->link;
-    }
+    khoiTao(list1);
+    khoiTao(list2);
+	Nodeptr p = list;
+	SinhVien dssv1,dssv2;
+	while (p != NULL) {
+		if (p->data.diem < 5)
+		{
+			dssv1 = p->data;
+			themCuoi(list1, dssv1);
+		}
+		else {
+			dssv2 = p->data;
+			themCuoi(list2, dssv2);
+		}
+		p = p->link;
+	}
 }
 
 // BTVN Cau 3
@@ -343,15 +346,16 @@ void nhapThemDiem(Nodeptr &list)
     Nodeptr p = list;
     while (p != NULL)
     {
-        cout << "Nhap them diem cho ma so sinh vien " << p->data.masv << endl;
-        cout << "Nhap diem toan: ";
+        cout << "Nhap them diem cho MSSV " << p->data.masv << endl;
+        cout << "Nhap diem Toan: ";
         cin >> p->data.toan;
-        cout << "Nhap diem ly: ";
+        cout << "Nhap diem Ly: ";
         cin >> p->data.ly;
-        cout << "Nhap diem hoa: ";
+        cout << "Nhap diem Hoa: ";
         cin >> p->data.hoa;
         p->data.dtb = (p->data.toan + p->data.ly + p->data.hoa) / 3;
-        cout << "Diem trung binh cua sinh vien la: " << p->data.dtb << endl;
+        cout << "Diem trung binh cua sinh vien " << p->data.tensv << " la: " << p->data.dtb << endl;
+        cout << endl;
         p = p->link;
     }
 }
