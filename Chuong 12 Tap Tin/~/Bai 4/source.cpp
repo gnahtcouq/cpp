124696a5baf8c7b5b8fbc52eb4c0c6cb7cf5309d
Quốc Thắng <comehere.thang@gmail.com>
2021-11-29 14:23:51 +0700

    29/11/21
    29/11/21

--- Chuong 12 Tap Tin/~/Bai 4/source.cpp
+++ Chuong 12 Tap Tin/~/Bai 4/source.cpp
@@ -10,3 +10,63 @@ OUTPUT.TXT
 8
 
 */
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+  // B1: Mo file
+  FILE *FileIn; // Khai bao con tro File
+  
+  // Cho con tro toi toi vung nho chua file INPUT.txt
+  FileIn = fopen("INPUT.txt", "r");
+
+  // Kiem tra xep tap tin co ton tai hay khong
+  if(!FileIn) {
+    cout << "\nKhong tim thay tap tin INPUT.txt . Hay kiem tra lai";
+    return 0;
+  }
+
+  // B2: Doc du lieu tu tap tin vao chuong trinh
+
+  // doc du lieu tu tap tin vao chuong trinh
+  // <=> nhap du lieu tu ban phim vao chuong trinh
+
+  // ghi du lieu tu chuong trinh ra tap tin
+  // <=> xuat du lieu ra man hinh
+
+  // eof => end of line
+
+
+  // Vong lap nay se lap lien tuc cho den cuoi file, neu chua den cuoi thi tiep tuc lap
+  int max;
+  fscanf(FileIn, "%d", &max); // doc so dau tien vao
+  while(!feof(FileIn)) {
+    int number;
+    fscanf(FileIn, "%d", &number);
+    
+    if(number > max)
+      max = number;
+  }
+
+  // B3: Dong tap tin
+  fclose(FileIn);
+
+
+
+  /* GHI DU LIEU VAO OUTPUT.txt */
+  // B1: Mo file
+  FILE *FileOut;
+  FileOut = fopen("OUTPUT.txt", "w"); // Tu dong tao ra moi neu chua co
+
+  // B2: Ghi file
+  fprintf(FileOut, "\nMax = %d", max);
+
+  // B3: Dong file
+  fclose(FileOut);
+
+  cout << endl;
+  return 0;
+}
