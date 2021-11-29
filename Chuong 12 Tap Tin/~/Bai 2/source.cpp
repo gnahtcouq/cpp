7b78fb2d64cf43859e467cc935660b411b7fa6fc
Quốc Thắng <comehere.thang@gmail.com>
2021-11-29 13:25:25 +0700

    29/11/21
    29/11/21

--- Chuong 12 Tap Tin/~/Bai 2/source.cpp
+++ Chuong 12 Tap Tin/~/Bai 2/source.cpp
@@ -1,6 +1,6 @@
-/* 
+/*
 
-Bài 2: 
+Bài  
 Cấu trúc file INPUT.TXT được tổ chức như sau:
 - Dòng đầu tiên là họ tên của sinh viên
 - Dòng tiếp theo lần lượt là điểm 3 môn toán, lý, hóa. Mỗi điểm cách nhau bởi một khoảng trắng
@@ -19,3 +19,78 @@ Xep Loai: Gioi
 
 
 */
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+float tinhDiemTrungBinh(float toan, float ly, float hoa) {
+  return (toan + ly + hoa) / 3;
+}
+
+char* xepLoai(float dtb) {
+  // 0 -> <2: Kem
+  // 2 -> <5: Yeu
+  // 5 -> <7: Trung binh
+  // 7 -> <8: Kha
+  // 8 -> <9: Gioi
+  // 9 -> <10: Xuat sac
+
+  if(dtb < 2)
+    return "Kem";
+  else if(dtb < 5)
+    return "Yeu";
+  else if(dtb < 7)
+    return "Trung Binh";
+  else if(dtb < 8)
+    return "Kha";
+  else if(dtb < 9)
+    return "Gioi";
+  return "Xuat Sac";
+}
+
+int main() {
+
+  // Mo file INPUT.txt va doc du lieu
+  // B1: Mo file
+  FILE* FileIn = fopen("INPUT.txt", "r");
+
+  // Kiem tra xem file co ton tai hay khong
+  if (!FileIn) {
+    cout << "\nKhong tim thay tap tin";
+    return 0;
+  }
+
+  // B2: Doc File
+  char ten[30];
+  float toan, ly, hoa;
+
+  fgets(ten, 30, FileIn);
+  fscanf(FileIn, "%f%f%f", &toan, &ly, &hoa);
+
+  // B3: Dong file
+  fclose(FileIn);
+
+  // cout << "\nTen: " << ten;
+  // cout << "\nToan = " << toan;
+  // cout << "\nLy = " << ly;
+  // cout << "\nHoa = " << hoa;
+
+
+  FILE* FileOut = fopen("OUTPUT.txt", "w");
+
+  fprintf(FileIn, "%s", ten);
+  float dtb = tinhDiemTrungBinh(toan, ly, hoa);
+  char xeploai[30];
+  strcpy(xeploai, xepLoai(dtb));
+
+  fprintf(FileOut, "\nDiem trung binh = %f", dtb);
+  fprintf(FileOut, "\nXep loai: %s", xeploai);
+  // cout << "\nDiem trung binh = " << dtb;
+  // cout << "\nXep loai: " << xeploai;
+
+  fclose(FileOut);
+
+  cout << endl;
+  return 0;
+}
