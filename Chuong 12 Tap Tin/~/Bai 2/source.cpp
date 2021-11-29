2669591462e4c6b504664f47b1a9c7e0f2f4cf33
Quốc Thắng <comehere.thang@gmail.com>
2021-11-29 13:04:59 +0700

    29/11/21
    29/11/21

---
+++ Chuong 12 Tap Tin/~/Bai 2/source.cpp
@@ -0,0 +1,21 @@
+/* 
+
+Bài 2: 
+Cấu trúc file INPUT.TXT được tổ chức như sau:
+- Dòng đầu tiên là họ tên của sinh viên
+- Dòng tiếp theo lần lượt là điểm 3 môn toán, lý, hóa. Mỗi điểm cách nhau bởi một khoảng trắng
+
+Yêu cầu: Đọc dữ liệu vào chương trình và tính điểm trung bình, xếp loại của sinh viên. Dữ liệu được ghi vào file OUTPUT.TXT
+
+vd:
+INPUT.TXT
+Nguyen Viet Nam Son
+7 8 9
+
+OUTPUT.TXT
+Nguyen Viet Nam Son
+Diem trung binh: 8
+Xep Loai: Gioi
+
+
+*/
