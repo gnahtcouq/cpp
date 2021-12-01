f096bedbff0e0f5f304954f2637b88317909f544
Quốc Thắng <comehere.thang@gmail.com>
2021-12-02 00:36:44 +0700

    2/12/21
    2/12/21

--- Chuong 15/source.cpp
+++ Chuong 15/source.cpp
@@ -18,32 +18,125 @@ struct ConVat {
 };
 typedef struct ConVat CONVAT;
 
-void nhapConVat(ifstream FileIn, CONVAT &cv) {
-  
+void nhapConVat(ifstream& FileIn, CONVAT& cv) {
+  FileIn >> cv.m_Code;
+  FileIn >> cv.m_Speed;
+  FileIn >> cv.m_Equiment;
+}
+
+void xuatConVat(ofstream& FileOut, CONVAT cv) {
+  FileOut << cv.m_Code << " ";
+  FileOut << cv.m_Speed << " ";
+  FileOut << cv.m_Equiment << " ";
+}
+
+void nhapDanhSach(ifstream& FileIn, vector<CONVAT>& danhSach) {
+  int n;
+  FileIn >> n;
+  for (int i = 0; i < n; i++) {
+    CONVAT cv;
+    nhapConVat(FileIn, cv);
+
+    danhSach.push_back(cv);
+  }
+}
+
+void xuatDanhSach(ofstream& FileOut, vector<CONVAT> danhSach) {
+  int n = danhSach.size();
+  for (int i = 0; i < n; i++) {
+    xuatConVat(FileOut, danhSach[i]);
+    FileOut << endl;
+  }
+}
+
+int timVanTocThucSu(CONVAT cv) {
+  if (cv.m_Equiment == 0)
+    return cv.m_Speed;
+  else {
+    if (cv.m_Code[0] == 'L')
+      return cv.m_Speed * 2;
+    else if (cv.m_Code[0] == 'R')
+      return cv.m_Speed + 30;
+    else if (cv.m_Code[0] == 'T')
+      return cv.m_Speed * 5;
+  }
+}
+
+double timThoiGianDiHetQuangDuong(CONVAT cv) {
+  int vantocthucsu = timVanTocThucSu(cv);
+  if (cv.m_Code[0] == 'L') {
+    return (double)SUTU_QUANGDUONGCHAY / vantocthucsu;
+  }
+  else if (cv.m_Code[0] == 'R') {
+    return (double)THO_QUANGDUONGCHAY / vantocthucsu;
+  }
+  else if (cv.m_Code[0] == 'T') {
+    return (double)RUA_QUANGDUONGCHAY / vantocthucsu;
+  }
+}
+
+double timThoiGianVeDich(CONVAT cv) {
+  double thoigianchay = timThoiGianDiHetQuangDuong(cv);
+  if (cv.m_Code[0] == 'L') {
+    return SUTU_THOIGIANBATDAU + thoigianchay;
+  }
+  else if (cv.m_Code[0] == 'R') {
+    return THO_THOIGIANBATDAU + thoigianchay;
+  }
+  else if (cv.m_Code[0] == 'T') {
+    return RUA_THOIGIANBATDAU + thoigianchay;
+  }
+}
+
+void hoanVi(CONVAT& a, CONVAT& b) {
+  CONVAT temp = a;
+  a = b;
+  b = temp;
+}
+
+void sapXepDanhSach(vector<CONVAT>& danhSach) {
+  int n = danhSach.size();
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = i + 1; j < n; j++) {
+      double thoigianvedich_1 = timThoiGianVeDich(danhSach[i]);
+      double thoigianvedich_2 = timThoiGianVeDich(danhSach[j]);
+      // if(thoigianvedich_1 <= 12 && thoigianvedich_2 <= 12)
+      if (thoigianvedich_1 > thoigianvedich_2)
+        hoanVi(danhSach[i], danhSach[j]);
+    }
+  }
 }
 
 int main() {
-  string m_Code;
-  int m_Speed, m_Equiment;
-  int n;
-  string temp;
-  ifstream FileIn;
-
-  FileIn.open("input.txt");
-  FileIn >> n;
-  cout << "\nSo luong: " << n;
-  for(int i = 0; i < n; i++) {
-    FileIn >> m_Code;
-    FileIn >> m_Speed;
-    FileIn >> m_Equiment;
-
-    cout << "\nm_Code = " << m_Code;
-    cout << "\nm_Speed = " << m_Speed;
-    cout << "\nm_Equiment = " << m_Equiment;
-    cout << "\n------------------";
+  ifstream FileIn("input.txt");
+  if (!FileIn) {
+    cout << "\nKhong tim thay tap tin";
+    return 0;
   }
-
+  // CONVAT cv;
+  vector<CONVAT> danhSach;
+  nhapDanhSach(FileIn, danhSach);
+  // nhapConVat(FileIn, cv);
   FileIn.close();
+
+  ofstream FileOut("output.txt");
+  sapXepDanhSach(danhSach);
+  int n = danhSach.size();
+  FileOut << " " << endl;
+  int dem = 0;
+  for (int i = 0; i < n; i++) {
+    if (timThoiGianVeDich(danhSach[i]) <= 12) {
+      FileOut << i + 1 << ". ";
+      xuatConVat(FileOut, danhSach[i]);
+      FileOut << endl;
+      dem++;
+    }
+  }
+  FileOut.seekp(0, FileOut.beg); // dich con tro chi ve ve dau
+  FileOut << dem << endl;
+  // xuatConVat(FileOut, cv);
+  // xuatDanhSach(FileOut, danhSach);
+  FileOut.close();
 
   system("pause");
   return 0;
