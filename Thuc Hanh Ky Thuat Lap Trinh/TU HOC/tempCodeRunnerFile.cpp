3b04ab4a4a8d3d87356e9988119186a1d0423ad6
Quốc Thắng <comehere.thang@gmail.com>
2021-12-04 16:17:34 +0700

    4/12/21
    4/12/21

--- Thuc Hanh Ky Thuat Lap Trinh/TU HOC/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-  nhapDanhSachNhanVien(ds);
-  if (ghiDanhSachNhanVien(ds, "danhsachnhanvien.dat"))
-    cout << "\nGhi file thanh cong" << endl;
-  else
-    cout << "\nGhi file khong duoc";
-  xuatDanhSachNhanVien(ds);
