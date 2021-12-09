a43612393304e8bf08c8935041deae7dcb0e37a7
Quốc Thắng <comehere.thang@gmail.com>
2021-12-09 12:09:47 +0700

    Create tinhngaycuame.cpp

---
+++ Chuong 7 Chuoi/remake/Bo Sung Kien Thuc/tinhngaycuame.cpp
@@ -0,0 +1,10 @@
+/*
+
+Ngay cua Me cong thuc nhu sau
+- Ngay chu nhat cua tuan le thu 2 cua thang 5 hang nam
+
+Nhap vao x (Voi x la so nam, yeu cau phai >= nam hien tai).
+Hay tinh xem ngay cua me trong nam x do la ngay bao nhieu?
+Yeu cau xuat ra ket qua o dang ngay/thang/nam
+
+*/
