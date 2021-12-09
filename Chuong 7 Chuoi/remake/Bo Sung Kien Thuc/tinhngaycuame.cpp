bf5b7e7f0062eb900f5b0f354bfc21a06641bd49
Quốc Thắng <comehere.thang@gmail.com>
2021-12-09 14:27:02 +0700

    9/12/21
    9/12/21

--- Chuong 7 Chuoi/remake/Bo Sung Kien Thuc/tinhngaycuame.cpp
+++ Chuong 7 Chuoi/remake/Bo Sung Kien Thuc/tinhngaycuame.cpp
@@ -7,4 +7,6 @@ Nhap vao x (Voi x la so nam, yeu cau phai >= nam hien tai).
 Hay tinh xem ngay cua me trong nam x do la ngay bao nhieu?
 Yeu cau xuat ra ket qua o dang ngay/thang/nam
 
+Goi y: Dung tu ngay hien tai, hay dua ve ngay chu nhat cua tuan hien tai, sau do lien tuc +7 de sang ngay chu nhat cua tuan sau, moi lan +7 neu vuot qua thang hien tai thi tang thang len va tru bot de ra so ngay cua thang sau, cu the cho den khi dieu kien dung la den thang 5 cua nam x (luc nay dem 2 lan ngay chu nhat, o lan thu 2 do chinh la ket qua)
+
 */
