8d8787613dda6d2d57f718ff3577d7167efb22dc
Quốc Thắng <comehere.thang@gmail.com>
2021-12-21 11:24:37 +0700

    21/12/21
    21/12/21

--- Thuc Hanh Nhap Mon Cau Truc Du Lieu/THI CUOI KY/3_Thang_DH52007101_CK.cpp
+++ Thuc Hanh Nhap Mon Cau Truc Du Lieu/THI CUOI KY/3_Thang_DH52007101_CK.cpp
@@ -14,7 +14,7 @@ struct CAYBUT {
 
 struct NODECAYBUT {
   CAYBUT data;
-  NODECAYBUT *link;
+  NODECAYBUT* link;
 };
 typedef NODECAYBUT* Nodeptr;
 
@@ -22,11 +22,11 @@ void khoiTao(Nodeptr &list);
 Nodeptr taoNode(CAYBUT x);
 Nodeptr themCuoi(Nodeptr &list, CAYBUT x);
 void giaiPhong(Nodeptr &list);
-bool kiemTraTrungMaCB(Nodeptr& list, int ma);
+bool kiemTraTrungMaCB(Nodeptr &list, int ma);
 void nhapDanhSachCayBut(Nodeptr &list);
 void xuatDanhSachCayBut(Nodeptr list);
-void sapXepDanhSachCayButTheoHangSX(Nodeptr &list);
-void sapXepGiamDanTheoDonGia(Nodeptr &list);
+void xuatTenHangTenButCoSoLuongKhongTonKho(Nodeptr list);
+void sapXepGiamDanTheoSoLuong(Nodeptr &list);
 
 // Phan 2
 struct NODE {
@@ -38,45 +38,44 @@ typedef NODE* NODEPTR;
 struct QUEUE {
   NODEPTR front, rear;
 };
-void init_Queue(QUEUE& q);
+void init_Queue(QUEUE &q);
 int isEmpty_Queue(QUEUE q);
 NODEPTR taoNode_Queue(int x);
-void enQueue(QUEUE& q, int x);
-void setValue_Queue(QUEUE& q);
+void enQueue(QUEUE &q, int x);
+void setValue_Queue(QUEUE &q);
 void print_Queue(QUEUE q);
-int deQueue(QUEUE& q);
+int deQueue(QUEUE &q);
 int getFront_Queue(QUEUE q);
-void chenSauNode(QUEUE& q, int x, int pos);
+void chenSauNode(QUEUE &q, int x, int pos);
 
 struct STACK {
   NODEPTR top;
 };
-void init_Stack(STACK& s);
+void init_Stack(STACK &s);
 int isEmpty_Stack(STACK s);
 NODEPTR taoNode_Stack(int x);
-void push(STACK& s, int a);
-int pop(STACK& s);
-int top(STACK& s);
-void input_Stack(STACK& s);
+void push(STACK &s, int a);
+int pop(STACK &s);
+int top(STACK &s);
+void input_Stack(STACK &s);
 void output_Stack(STACK s);
-void xoaPhanTuXThu2TroDi(STACK& s, int x);
+void xoaPhanTuXThu2TroDi(STACK &s, int x);
 
 int main() {
   // Phan 1 - Cau 1
   Nodeptr ds;
   nhapDanhSachCayBut(ds);
   xuatDanhSachCayBut(ds);
-  // Phan 1 - Cau 2
-  sapXepDanhSachCayButTheoHangSX(ds);
-  cout << "\nDanh sach sau khi sap xep giam dan theo hang san xuat";
-  xuatDanhSachCayBut(ds);
 
+  // Phan 1 - Cau 2
+  cout << "\nTen Hang SX va Ten But co so luong ton kho bang 0" << endl;
+  xuatTenHangTenButCoSoLuongKhongTonKho(ds);
 
   // Phan 1 - Cau 3
-  sapXepGiamDanTheoDonGia(ds);
-  cout << "\nDanh sach sau khi sap xep giam dan theo don gia";
+  cout << "\nSap xep giam theo so luong cay but";
+  sapXepGiamDanTheoSoLuong(ds);
   xuatDanhSachCayBut(ds);
-  cout << endl;
+  cout << "\n\n";
 
   // Phan 2 - Cau 1
   QUEUE q;
@@ -103,7 +102,7 @@ int main() {
   cout << "\nNhap phan tu can xoa: ";
   cin >> x2;
   xoaPhanTuXThu2TroDi(s, x2);
-  cout << "\nStack sau khi xoa: ";
+  cout << "Stack sau khi xoa: ";
   output_Stack(s);
 
   system("pause");
@@ -147,7 +146,7 @@ void giaiPhong(Nodeptr &list) {
   }
 }
 
-bool kiemTraTrungMaCB(Nodeptr& list, int ma) {
+bool kiemTraTrungMaCB(Nodeptr &list, int ma) {
   Nodeptr p = list;
   while (p != NULL) {
     if (p->data.maCB == ma)
@@ -157,7 +156,7 @@ bool kiemTraTrungMaCB(Nodeptr& list, int ma) {
   return false;
 }
 
-// Cau 1
+// Phan 1 - Cau 1
 void nhapDanhSachCayBut(Nodeptr &list) {
   CAYBUT cb;
   khoiTao(list);
@@ -190,7 +189,7 @@ void xuatDanhSachCayBut(Nodeptr list) {
   cout << "\n\n\tDANH SACH CAC CAY BUT" << endl;
   cout << setw(10) << left << "Ma CB" << "\t";
   cout << setw(30) << left << "Ten CB" << "\t";
-  cout << setw(15) << left << "So luong" << "\t" ;
+  cout << setw(15) << left << "So luong" << "\t";
   cout << setw(15) << left << "Don gia" << "\t";
   cout << setw(30) << left << "Hang san xuat" << "\t" << endl;
   while (list != NULL) {
@@ -199,46 +198,38 @@ void xuatDanhSachCayBut(Nodeptr list) {
     cout << setw(15) << left << list->data.soLuong << "\t";
     cout << setw(15) << left << list->data.donGia << "\t";
     cout << setw(30) << left << list->data.hangSX << "\t" << endl;
-  list = list->link;
+    list = list->link;
   }
-  
+
 }
 
-// Cau 2
-void sapXepDanhSachCayButTheoHangSX(Nodeptr &list) {
-  for (Nodeptr pTmp = list; pTmp != NULL; pTmp = pTmp->link) {
-    for (Nodeptr pTmp2 = pTmp->link; pTmp2 != NULL; pTmp2 = pTmp2->link) {
-      if (strcmp(pTmp->data.hangSX, pTmp2->data.hangSX) > 0) {
+// Phan 1 - Cau 2
+void xuatTenHangTenButCoSoLuongKhongTonKho(Nodeptr list) {
+  if (list != NULL) {
+    Nodeptr p = list;
+    while (p != NULL) {
+      if (p->data.soLuong == 0) {
+        cout << "\tTen hang " << p->data.hangSX << ", ten but " << p->data.tenCB << endl;
+      }
+      p = p->link;
+    }
+  }
+}
+
+// Phan 1 - Cau 3
+void sapXepGiamDanTheoSoLuong(Nodeptr &list) {
+  for (Nodeptr pTmp = list; pTmp != NULL; pTmp = pTmp->link)
+    for (Nodeptr pTmp2 = pTmp->link; pTmp2 != NULL; pTmp2 = pTmp2->link)
+      if (pTmp->data.soLuong < pTmp2->data.soLuong) {
         CAYBUT tmp = pTmp->data;
         pTmp->data = pTmp2->data;
         pTmp2->data = tmp;
       }
-    }
-  }
-  
 }
 
-// Cau 3
-// void sapXepGiamDanTheoDonGia(Nodeptr &list) {
-//   // for (Nodeptr pTmp = list; pTmp != NULL; pTmp = pTmp->link)
-//   //   for (Nodeptr pTmp2 = pTmp->link; pTmp2 != NULL; pTmp2 = pTmp2->link)
-//   //     if (pTmp->data.donGia < pTmp2->data.donGia) {
-//   //       CAYBUT tmp = pTmp->data;
-//   //       pTmp->data = pTmp2->data;
-//   //       pTmp2->data = tmp;
-//   //     }
-//   if (list != NULL) {
-//     Nodeptr p = list;
-//     while (p != NULL) {
-//       cout << "Hang SX " << p->data.hangSX << ", so luong = " << p->data.soLuong << endl;
-//       p = p->link;
-//     }
-//   }
-// }
-
 // Phan 2
-// Cau 1
-void init_Queue(QUEUE& q) {
+// Phan 1 - Cau 1
+void init_Queue(QUEUE &q) {
   q.front = NULL;
   q.rear = NULL;
 }
@@ -257,7 +248,7 @@ NODEPTR taoNode_Queue(int x) {
   return p;
 }
 
-void enQueue(QUEUE& q, int x) {
+void enQueue(QUEUE &q, int x) {
   NODEPTR p;
   p = taoNode_Queue(x);
   if (isEmpty_Queue(q)) {
@@ -270,7 +261,7 @@ void enQueue(QUEUE& q, int x) {
   }
 }
 
-void setValue_Queue(QUEUE& q) {
+void setValue_Queue(QUEUE &q) {
   int x;
   while (1) {
     cout << "Nhap gia tri cho Queue (nhap -99 de dung): ";
@@ -290,7 +281,7 @@ void print_Queue(QUEUE q) {
   }
 }
 
-int deQueue(QUEUE& q) {
+int deQueue(QUEUE &q) {
   if (isEmpty_Queue(q))
     return -1;
   else {
@@ -309,7 +300,7 @@ int getFront_Queue(QUEUE q) {
   return x;
 }
 
-void chenSauNode(QUEUE& q, int x, int pos) {
+void chenSauNode(QUEUE &q, int x, int pos) {
   QUEUE tam;
   QUEUE p = q;
   init_Queue(tam);
@@ -325,7 +316,7 @@ void chenSauNode(QUEUE& q, int x, int pos) {
 }
 
 // Phan 2 - Cau 2
-void init_Stack(STACK& s) {
+void init_Stack(STACK &s) {
   s.top = NULL;
 }
 
@@ -341,7 +332,7 @@ NODEPTR taoNode_Stack(int x) {
   return p;
 }
 
-void push(STACK& s, int a) {
+void push(STACK &s, int a) {
   NODEPTR p;
   p = taoNode_Stack(a);
   if (isEmpty_Stack(s))
@@ -352,21 +343,20 @@ void push(STACK& s, int a) {
   }
 }
 
-void push_top(STACK& s, int a) {
-	NODEPTR p;
-	p = taoNode_Stack(a);
-	if (isEmpty_Stack(s))
-		s.top = p;
-	else
-	{
-		NODEPTR q = s.top;
-		while (q->link != NULL)
-			q = q->link;
-		q->link = p;
-	}
+void push_top(STACK &s, int a) {
+  NODEPTR p;
+  p = taoNode_Stack(a);
+  if (isEmpty_Stack(s))
+    s.top = p;
+  else {
+    NODEPTR q = s.top;
+    while (q->link != NULL)
+      q = q->link;
+    q->link = p;
+  }
 }
 
-void input_Stack(STACK& s) {
+void input_Stack(STACK &s) {
   int x;
   while (1) {
     cout << "Nhap gia tri cho Stack (Nhap -99 de dung): ";
@@ -385,7 +375,7 @@ void output_Stack(STACK s) {
   }
 }
 
-int pop(STACK& s) {
+int pop(STACK &s) {
   NODEPTR p;
   int a;
   if (!isEmpty_Stack(s)) {
@@ -398,7 +388,7 @@ int pop(STACK& s) {
   return -1;
 }
 
-int top(STACK& s) {
+int top(STACK &s) {
   NODEPTR p;
   int a;
   if (!isEmpty_Stack(s)) {
@@ -408,21 +398,21 @@ int top(STACK& s) {
   return -1;
 }
 
-void xoaPhanTuXThu2TroDi(STACK& s, int x) {
-  STACK tam;
+void xoaPhanTuXThu2TroDi(STACK &s, int x) {
   STACK p = s;
-  init_Stack(tam);
+  STACK q;
+  init_Stack(q);
   int check = 0;
   while (!isEmpty_Stack(p)) {
     if (top(p) != x)
-      push_top(tam, pop(p));
+      push_top(q, pop(p));
     else {
       if (check == 0)
-        push_top(tam, pop(p));
+        push_top(q, pop(p));
       check++;
       if (check > 1)
         pop(p);
     }
   }
-  s = tam;
+  s = q;
 }
