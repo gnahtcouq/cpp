9d782de5fdcc73206bef0c1f5007d5f1529ba126
Quốc Thắng <comehere.thang@gmail.com>
2022-01-09 12:11:53 +0700

    9/1/22
    9/1/22

---
+++ Nhap Mon Cau Truc Du Lieu/onthicuoiky.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+// Câu 2
+const int MAX = 50;       // Chiều dài tối đa của danh sách đặc là 50
+struct DAYSO {
+  int n;                  // Số phần tử trong danh sách
+  int ds[MAX];
+};
+
+
+int main() {
+
+  // Câu 1
+  // Danh sách đặc là danh sách 
+  // A. Các phần tử được lưu trữ kề nhau trong bộ nhớ
+  //
+  //
+  //
+  // Câu 2
+  // C.
+  //
+  //
+  //
+  // Câu 3
+  // Trong phép toán chèn 1 phần tử vào danh sách, vị trí chèn pos nào sau đây là hợp lệ
+  // if (pos < 0 || pos > n)
+  //   cout << "\n Vi tri khong hop le";
+  // D. 0 < pos < n
+  //
+  //
+  //
+  // Câu 4
+  // Trong phép toán loại bỏ 1 phần tử khỏi danh sách, vị trí pos nào sau đây là hợp lệ
+  // if (pos < 0 || pos > n)
+  //   cout << "\n Vi tri khong hop le";
+  // D. 0 < pos < n
+  //
+  //
+  //
+  // Câu 5
+  // Đối với thuật toán tìm kiếm nhị phân
+  // Danh sách phải có thứ tự tăng dần (hoặc giảm dần)
+  // A. Danh sách phải có thứ tự
+
+  system("pause");
+  return 0;
+}
