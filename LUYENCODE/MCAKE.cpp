dc48a23e3024dadc3210b3f689e60c4f39e90067
Quốc Thắng <comehere.thang@gmail.com>
2022-01-21 15:57:39 +0700

    21/1/22
    21/1/22

--- LUYENCODE/MCAKE.cpp
+++ LUYENCODE/MCAKE.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
@@ -7,39 +8,42 @@ int main() {
   for (int i = 0; i < n * 2; i++)
     cin >> a[i];
 
-  cout << "\n";
   int dukien[100000], d = 0;
   for (int i = 0; i < n * 2; i++)
     if (i % 2 != 0)
-      dukien[d++] = a[i] - a[i - 1];
+      dukien[d++] = a[i] - a[i - 1] + 1;
 
+  int max_dukien = dukien[0];
   for (int i = 0; i < d; i++)
-    cout << dukien[i] << " ";
-
-  cout << "\n";
+    if (dukien[i] > max_dukien)
+      max_dukien = dukien[i];
 
   for (int i = 0; i < L; i++)
     b[i] = 0;
 
-  for (int i = 0; i < L; i++)
-    cout << b[i] << " ";
-
-  int dem = n + 1;
+  int lop = n + 1;
   for (int i = (n * 2) - 1; i >= 0; i--) {
     if (i % 2 == 0) {
-      dem--;
+      lop--;
       for (int j = a[i] - 1; j < a[i + 1]; j++)
         for (int k = 0; k < L; k++)
-          b[j] = dem;
+          b[j] = lop;
     }
   }
 
-  cout << "\n";
+  sort(b, b + L);
+  int max_thucte = 0, dem_max_thucte = 1;
+  for (int i = 0; i < L; i++) {
+    if (b[i] != 0 && b[i] == b[i + 1]) {
+      dem_max_thucte++;
+      if (dem_max_thucte > max_thucte)
+        max_thucte = dem_max_thucte;
+    }
+    else
+      dem_max_thucte = 1;
+  }
 
-  for (int i = 0; i < L; i++)
-    cout << b[i] << " ";
-
-
+  cout << max_dukien << " " << max_thucte;
 
   return 0;
 }
