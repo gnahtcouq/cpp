b6491c126303661eab555c6252e5b7401d8f9ace
Quốc Thắng <comehere.thang@gmail.com>
2022-01-22 23:14:57 +0700

    22/1/22
    22/1/22

--- LUYENCODE/SODEPN.cpp
+++ LUYENCODE/SODEPN.cpp
@@ -2,23 +2,15 @@
 using namespace std;
 
 int main() {
-  int n, c;
+  int n, tong = 0;
   cin >> n;
 
-  int b[100000], m = 0;
   while (n != 0) {
-    c = n % 10;
+    tong += n % 10;
     n /= 10;
-    b[m++] = c;
   }
 
-  int tong = 0;
-  for (int i = 0; i < m; i++)
-    tong += b[i];
-
-  int check = tong % 10;
-
-  if (check == 9)
+  if (tong % 10 == 9)
     cout << "YES";
   else
     cout << "NO";
